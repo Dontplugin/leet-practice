22275cf22eca87034332769c8c9b93f247ab40e1
韩宇哲 <hanyuzhe@souche.com>
2019-05-09 10:19:51 +0800

    feat: 增加一些链表的

---
+++ src/main/java/linkedList/设计链表_707.java
@@ -0,0 +1,154 @@
+package linkedList;
+
+/**
+ * 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。
+ * val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性
+ * prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+ *
+ * 在链表类中实现这些功能：
+ *
+ * get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ * addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ * addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。
+ * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+ *
+ *
+ * 示例：
+ *
+ * MyLinkedList linkedList = new MyLinkedList();
+ * linkedList.addAtHead(1);
+ * linkedList.addAtTail(3);
+ * linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+ * linkedList.get(1);            //返回2
+ * linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+ * linkedList.get(1);            //返回3
+ *
+ *
+ * 提示：
+ *
+ * 所有值都在 [1, 1000] 之内。
+ * 操作次数将在  [1, 1000] 之内。
+ * 请不要使用内置的 LinkedList 库。
+ */
+public class 设计链表_707 {
+
+    /**
+     * 执行用时 : 117 ms, 在Design Linked List的Java提交中击败了73.96% 的用户
+     * 内存消耗 : 47.6 MB, 在Design Linked List的Java提交中击败了69.32% 的用户
+     * @param args
+     */
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.addAtIndex(0, 10);
+        linkedList.addAtIndex(0, 20);
+        linkedList.addAtIndex(1, 30);
+        linkedList.get(0);
+        System.out.println();
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode prev;
+        ListNode(int x) { val = x; }
+    }
+    static class MyLinkedList {
+        int size;
+        ListNode first;
+        ListNode last;
+
+        /** Initialize your data structure here. */
+        public MyLinkedList() {
+            first = null;
+            last = null;
+            size = 0;
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int get(int index) {
+            if (index + 1 > size || index < 0) return -1;
+            ListNode cur = first;
+            while (index-- > 0) {
+                cur = cur.next;
+            }
+
+            return cur.val;
+
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void addAtHead(int val) {
+            ListNode newFirst = new ListNode(val);
+            if (first == null) {
+                first = newFirst;
+                last = newFirst;
+            } else {
+                first.prev = newFirst;
+                newFirst.next = first;
+                first = newFirst;
+            }
+            size++;
+        }
+
+        /** Append a node of value val to the last element of the linked list. */
+        public void addAtTail(int val) {
+            ListNode newTail = new ListNode(val);
+
+            if (last == null) {
+                last = newTail;
+                first = newTail;
+            } else {
+                last.next = newTail;
+                newTail.prev = last;
+                last = newTail;
+            }
+            size++;
+        }
+
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void addAtIndex(int index, int val) {
+            if (index <= 0) {
+                addAtHead(val);
+                return;
+            }
+            if (index == size) {
+                addAtTail(val);
+                return;
+            }
+            if (index + 1 > size) return;
+            ListNode value = new ListNode(val);
+            ListNode cur = first;
+            while (index-- > 0) {
+                cur = cur.next;
+            }
+            ListNode lastNode = cur.prev;
+            lastNode.next = value;
+            value.prev = lastNode;
+            value.next = cur;
+            cur.prev = value;
+            size++;
+        }
+
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void deleteAtIndex(int index) {
+            if (index + 1 > size || index < 0) return;
+            ListNode cur = first;
+            while (index-- > 0) {
+                cur = cur.next;
+            }
+            ListNode before = cur.prev;
+            ListNode behind = cur.next;
+            if (before != null)
+                before.next = behind;
+            else
+                first = behind;
+
+            if (behind != null)
+                behind.prev = before;
+            else
+                last = before;
+            size--;
+        }
+    }
+}
---
+++ src/main/java/linkedList/链表中的下一个更大节点_1019.java
@@ -0,0 +1,153 @@
+package linkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给出一个以头节点 head 作为第一个节点的链表。链表中的节点分别编号为：node_1, node_2, node_3, ... 。
+ *
+ * 每个节点都可能有下一个更大值（next larger value）：对于 node_i，如果其 next_larger(node_i)
+ * 是 node_j.val，那么就有 j > i 且  node_j.val > node_i.val，而 j 是可能的选项中最小的那个。如果不存在这样的 j，那么下一个更大值为 0 。
+ *
+ * 返回整数答案数组 answer，其中 answer[i] = next_larger(node_{i+1}) 。
+ *
+ * 注意：在下面的示例中，诸如 [2,1,5] 这样的输入（不是输出）是链表的序列化表示，其头节点的值为 2，第二个节点值为 1，第三个节点值为 5 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[2,1,5]
+ * 输出：[5,5,0]
+ * 示例 2：
+ *
+ * 输入：[2,7,4,3,5]
+ * 输出：[7,0,5,5,0]
+ * 示例 3：
+ *
+ * 输入：[1,7,5,1,9,2,5,1]
+ * 输出：[7,9,9,9,0,5,0,0]
+ */
+public class 链表中的下一个更大节点_1019 {
+
+    public static void main(String[] args) {
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(7);
+        l2.next.next = new ListNode(4);
+        l2.next.next.next = new ListNode(3);
+        l2.next.next.next.next = new ListNode(5);
+        System.out.println(nextLargerNodes(l2));
+    }
+
+    // 使用 monotonic stack 解决此类问题。
+    public static int[] nextLargerNodes(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        // 1. 创建一个容器 list 来存储数据
+        ArrayList<Integer> list = new ArrayList<>();
+        int size = 0;
+        while (head != null) {
+            list.add(head.val);
+            size++;
+            head = head.next;
+        }
+        // 2. 创建一个栈 stack ，这个栈里面存储的是对应位置的 list 元素及其之后元素中最大的值。
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[size];
+        // 3. 在 list 中从右往左遍历，stack 中凡是比 list.get(i) 小的都 pop 出去，
+        // 这样 stack 剩下的元素都是比 list.get(i) 更大的元素。
+        for (int i = list.size() - 1; i >= 0; i--) {
+            while (!stack.empty() && stack.peek() <= list.get(i)) {
+                stack.pop();
+            }
+            ans[i] = stack.empty() ? 0 : stack.peek();
+            stack.push(list.get(i));
+        }
+        return ans;
+    }
+    /*public static int[] nextLargerNodes(ListNode head) {
+        int[] stack = new int[10000];
+        int[] res = new int[10000];
+        int[] temp = new int[10000];
+        int top = -1, i = 0;
+        ListNode node = head;
+        while (node != null) {
+            while (top != -1 && temp[stack[top]] < node.val)
+                res[stack[top--]] = node.val;
+            stack[++top] = i;
+            temp[i++] = node.val;
+            node = node.next;
+        }
+        return Arrays.copyOf(res, i);
+    }*/
+    /**
+     * 执行用时 : 130 ms, 在Next Greater Node In Linked List的Java提交中击败了34.18% 的用户
+     * 内存消耗 : 39.6 MB, 在Next Greater Node In Linked List的Java提交中击败了100.00% 的用户
+     * @param head
+     * @return
+     */
+    /*public static int[] nextLargerNodes(ListNode head) {
+        int[] temp = new int[10000];
+        int count = 0;
+        while (head != null) {
+            temp[count++] = head.val;
+            head = head.next;
+        }
+
+        int[] ret = new int[count];
+        for (int i = 0; i < count; i++) {
+            ret[i] = getLarger(temp, count, i);
+        }
+
+        return ret;
+    }
+
+    public static int getLarger(int[] all, int size, int target) {
+        int value = all[target];
+        for (int i = target + 1; i < size; i++) {
+            int nextValue = all[i];
+            if (nextValue > value) {
+                return nextValue;
+            }
+        }
+        return 0;
+
+    }*/
+
+    // 超时
+    /*public static int[] nextLargerNodes(ListNode head) {
+
+        List<Integer> list = new ArrayList<>();
+        ListNode cur = head;
+
+        while (head != null) {
+            int value = head.val;
+            while (cur.next != null) {
+                int nextValue = cur.next.val;
+                if (nextValue > value) {
+                    list.add(nextValue);
+                    break;
+                }
+                cur = cur.next;
+            }
+
+            if (cur.next == null)
+                list.add(0);
+
+            head = head.next;
+            cur = head;
+        }
+
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }*/
+    
+}
